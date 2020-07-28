e977866aeb6854c1aad5a001aae74452648586ad
OscarBorges <oscar.borgesfs@gmail.com>
2020-07-28 18:20:57 -0300

    Criados e removidos atributos dinâmicos de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -8,12 +8,21 @@ class Pessoa:
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    elton = Pessoa( nome = 'elton')
+    elton = Pessoa(nome = 'elton')
     oscar = Pessoa(elton, nome = 'oscar')
+    elton = Pessoa(oscar, nome = 'elton')
     print(Pessoa.cumprimentar(oscar))
     print(id(oscar))
     print(oscar.cumprimentar())
     print(oscar.nome)
     print(oscar.idade)
+    for irmaos in elton.irmaos:
+        print(irmaos.nome)
     for irmaos in oscar.irmaos:
         print(irmaos.nome)
+    oscar.sobrenome = 'borges'
+    elton.sobrenome = 'faletinha'
+    elton.idade = 25
+    del elton.sobrenome
+    print(oscar.__dict__)
+    print(elton.__dict__)
